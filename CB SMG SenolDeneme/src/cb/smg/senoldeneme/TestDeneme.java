1150fc6ec9d82141cb53dc033ce8336536d73659
senolb <bayramsenol@yahoo.com>
2026-03-04 12:10:14 +0300

    Update TestDeneme.java

--- CB SMG SenolDeneme/src/cb/smg/senoldeneme/TestDeneme.java
+++ CB SMG SenolDeneme/src/cb/smg/senoldeneme/TestDeneme.java
@@ -6,6 +6,6 @@ public class TestDeneme {
 		// TODO Auto-generated method stub
 		System.out.println("deneme9");
 		System.out.println("deneme9");
-		System.out.println("deneme20");
+		System.out.println("deneme21");
 	}
 }
